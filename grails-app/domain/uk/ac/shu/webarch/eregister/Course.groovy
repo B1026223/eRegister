e6ee08a4a4d18d1ae3883f23919e2183d8dbb239
Nikhil Raja <b1026223@my.shu.ac.uk>
2013-02-18 16:23:46 +0000

    Added strings to the Course domain

--- grails-app/domain/uk/ac/shu/webarch/eregister/Course.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Course.groovy
@@ -3,7 +3,19 @@ package uk.ac.shu.webarch.eregister
 class Course {
 
 	String courseName
+	String courseCode
+	String courseDescription
+
+	static hasMany = [
+	name_of_classes:RegClass
+	]
+	
+	static mappedBy = [
+	name_of_classes:'Course'
+	]
+
 
     static constraints = {
     }
+
 }
