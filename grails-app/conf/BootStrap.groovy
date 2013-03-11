8f22e077e89dfb48e4fd0c0f4bd9070f7107a4d8
Nikhil Raja <b1026223@my.shu.ac.uk>
2013-03-11 18:14:08 +0000

    created my first bootstrap so instructor has default values already in the table. Still needs work...

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,24 @@
+import uk.ac.shu.webarch.eregister.*;
 class BootStrap {
 
     def init = { servletContext ->
-    }
+
+	println ("BootStrap::init");
+
+	def frankie_instructor = Instructor.findByStaffNumber('0001');
+	
+	if ( frankie_instructor == null ) {
+		println("Instuctor not found, create...");
+		frankie_instructor = new Instructor ()
+		frankie_instructor.StaffNumber = 0001;
+		frankie_instructor.name('Frankie Preston');
+		frankie_instructor.save();
+	}
+	else {
+		println("Result of find by staff number for Frankie: ${frankie_instructor}");
+
+    	     }
+	}
     def destroy = {
     }
 }
