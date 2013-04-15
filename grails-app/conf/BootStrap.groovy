78eec594c7402b4e09acdfa86982700ab4a646a5
Nikhil Raja <b1026223@my.shu.ac.uk>
2013-04-15 17:47:21 +0100

    Added a course default value so it shows on the course link

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,8 +5,8 @@ class BootStrap {
 
 	println ("BootStrap::init");
 
-	def frankie_instructor = Instructor.findByStaffNumber('0001') ?: new Instructor(staffNumber:'0001', name:'Frankie 	  Preston').save();
-	def rob_instructor = Instructor.findByStaffNumber('0002') ?: new Instructor(staffNumber:'0002', name:'Rob 		Wallace').save();
+	def frankie_instructor = Instructor.findByStaffNumber('0001') ?: new Instructor(staffNumber:'0001', name:'Frankie 	  Preston').save(failOnError:true);
+	def rob_instructor = Instructor.findByStaffNumber('0002') ?: new Instructor(staffNumber:'0002', name:'Rob 		Wallace').save(failOnError:true);
 
 	def webArch_course = Course.findByCourseCode('0001a') ?: new Course(courseCode:'0001a', 
 							         courseName:'Web Architectures',
@@ -15,15 +15,15 @@ class BootStrap {
 							         courseName:'Database Systems',
 							         courseDescription:'A not so fun and exciting 									 module').save();
 
-	def new_class = new RegClass(name:'WebArch-Mondays', class_instructor:frankie_instructor, 
-							     courseOwner:webArch_course).save();
-
 	def ian_student = Student.findByStudentNumber('1234') ?: new Student(studentNumber:'1234', 
 								 studentName:'Ian  Ibbotson').save();
 	def matthew_student = Student.findByStudentNumber('5555') ?: new Student(studentNumber:'5555', 
 								     studentName:'Mathhew Love').save();
 
-
+	def new_class = RegClass.findByName('WebArch-Mondays') ?: new RegClass (name:'WebArch-Mondays', 								  class_instructor:frankie_instructor, 
+							          courseOwner:webArch_course).save();
+	def new_class = RegClass.findByName('DatabaseSystems-Wednesday') ?: new RegClass (name:'DatabaseSystems-Wednesday', 								  	class_instructor:rob_instructor, 
+							          courseOwner:databaseSystems_course).save();
 	}
     def destroy = {
     }
